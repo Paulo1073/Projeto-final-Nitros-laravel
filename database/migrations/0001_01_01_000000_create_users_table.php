76608595789aa4ce6e003e1cb6c490fba1eeea09
Paulo1073 <168665678+Paulo1073@users.noreply.github.com>
2025-10-11 09:38:08 -0300

    Criação da tabela 'users' com os campos 'nickname' e 'bio'

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('password');
+            $table->string('nickname');
+            $table->text('bio');
             $table->timestamps();
         });
 
