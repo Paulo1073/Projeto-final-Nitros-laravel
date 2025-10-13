969e651a7f6944477bd1fb2d0cdf06460e5fe41d
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-13 16:13:51 -0300

    Criação de dois componentes, um para cada logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,1 @@
-<img class="h-48 w-48" src="{{ asset('assets/images/Logo2-sf.png') }}" alt="">
-
+<img {{ $attributes->merge(['class' => 'h-48 w-48']) }} src="{{ asset('assets/images/Logo2-sf.png') }}" alt="">
