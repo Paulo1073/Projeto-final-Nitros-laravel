1b35a0ff12a382f5591dde7e1b4da7e88428b376
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-13 20:04:04 -0300

    feat: Cria a tabela 'games' no Banco de Dados do projeto

--- database/migrations/2025_10_13_222455_create_games_table.php
+++ database/migrations/2025_10_13_222455_create_games_table.php
@@ -6,22 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('games', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('jogos', function (Blueprint $table) {
+            $table->id(); // id auto incremental
+            $table->string('titulo'); // título do jogo
+            $table->string('genero')->nullable(); // gênero (ex: ação, aventura, etc.)
+            $table->text('descricao')->nullable(); // descrição do jogo
+            $table->string('plataforma')->nullable(); // plataforma (ex: PC, PS5, Xbox)
+            $table->string('imagem')->nullable(); // caminho da imagem/capa do jogo
+            $table->timestamps(); // created_at e updated_at
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('games');
+        Schema::dropIfExists('jogos');
     }
 };
