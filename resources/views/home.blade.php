02cc1f8a7c6a598da21e3d16184a9372e8793ee1
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-13 15:23:14 -0300

    Mudança na logo e icones do projeto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="theme-color" content="#000000" /> <!-- Cor da barra no mobile -->
+
   <title>Nitros</title>
-  <link rel="icon"  type="image/x-icon" href="{{ asset('assets/images/icon.ico') }}">
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Nitros" />
+  <link rel="manifest" href="/site.webmanifest" />
+
+  
+
+
   @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+</head
+
 <body class="m-0 p-0 box-border relative">
   <nav class=" gap-6 ml-190 flex justify-end items-end absolute top-14  w-100 z-50 bg-transparent">
     @auth
