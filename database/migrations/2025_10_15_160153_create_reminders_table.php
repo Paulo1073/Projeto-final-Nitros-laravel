762210ca74af9a7c66b22605fbc214c7b26d11b7
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-15 13:06:31 -0300

    Feat:Cria a tabela 'reminders' no Banco de Dados

--- database/migrations/2025_10_15_160153_create_reminders_table.php
+++ database/migrations/2025_10_15_160153_create_reminders_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('reminders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('game_id')->constrained('games')->onDelete('cascade'); 
+            $table->string('titulo')->nullable(); 
+            $table->text('descricao'); 
+            $table->boolean('concluido')->default(false); 
+            $table->timestamps(); 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('reminders');
