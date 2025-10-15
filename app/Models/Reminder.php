9e275f989d8d4d66cf10ce3563bf51da3877fdae
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-15 13:08:53 -0300

    Feat:Registra na model quais campos podem ser preenchidos

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reminder extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'game_id',
+        'titulo',
+        'descricao',
+        'concluido',
+    ];
+
+    // Relações
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
 }
