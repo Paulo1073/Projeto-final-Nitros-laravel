7079a4767f78a2ff07510debdd8346dbd00de902
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-16 08:48:37 -0300

    feat:Registra na model quais campos podem ser preenchidos

--- app/Models/Speedrun.php
+++ app/Models/Speedrun.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Speedrun extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'game_id',
+        'tempo',
+        'modo',
+        'data'
+    ];
 }
