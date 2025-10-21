cf6beb99ea9bc4a5cdd44b56e73f0077b271db68
Paulo <Paulo.silva1073@aluno.ce.gov.br>
2025-10-21 19:48:39 -0300

    chore(images): adiciona upload de imagem para background de login e cadastro

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,39 +1,37 @@
-<div class="relative w-[1525px] mx-auto h-[557px] overflow-hidden  ">
+<div class="relative w-[1477px] mx-auto h-[572px] overflow-hidden  ">
     <!-- Slides -->
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-100" id="slide1">
-        <img src="{{ asset('assets/images/10.png') }}" class="w-full h-full object-cover" alt="Banner 1">
+        <img src="{{ asset('assets/images/4.png') }}" class="w-full h-full object-cover" alt="Banner 1">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide2">
-        <img src="{{ asset('assets/images/11.png') }}" class="w-full h-full object-cover" alt="Banner 2">
+        <img src="{{ asset('assets/images/5.png') }}" class="w-full h-full object-cover" alt="Banner 2">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide3">
-        <img src="{{ asset('assets/images/12.png') }}" class="w-full h-full object-cover" alt="Banner 3">
+        <img src="{{ asset('assets/images/6.png') }}" class="w-full h-full object-cover" alt="Banner 3">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide4">
-        <img src="{{ asset('assets/images/13.png') }}" class="w-full h-full object-cover" alt="Banner 4">
+        <img src="{{ asset('assets/images/7.png') }}" class="w-full h-full object-cover" alt="Banner 4">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide5">
-        <img src="{{ asset('assets/images/14.png') }}" class="w-full h-full object-cover" alt="Banner 5">
+        <img src="{{ asset('assets/images/8.png') }}" class="w-full h-full object-cover" alt="Banner 5">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide6">
-        <img src="{{ asset('assets/images/15.png') }}" class="w-full h-full object-cover" alt="Banner 6">
+        <img src="{{ asset('assets/images/9.png') }}" class="w-full h-full object-cover" alt="Banner 6">
     </div>
 
     <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide7">
-        <img src="{{ asset('assets/images/16.png') }}" class="w-full h-full object-cover" alt="Banner 7">
-    </div>
-
-    <div class="absolute inset-0 transition-opacity duration-1000 opacity-0" id="slide8">
-        <img src="{{ asset('assets/images/17.png') }}" class="w-full h-full object-cover" alt="Banner 8">
+        <img src="{{ asset('assets/images/10.png') }}" class="w-full h-full object-cover" alt="Banner 7">
     </div>
 
 
-    <!-- Indicadores -->
+
+
+
     <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         <div class="w-3 h-3 bg-white rounded-full opacity-70" id="dot1"></div>
         <div class="w-3 h-3 bg-white rounded-full opacity-40" id="dot2"></div>
@@ -42,7 +40,6 @@
         <div class="w-3 h-3 bg-white rounded-full opacity-40" id="dot5"></div>
         <div class="w-3 h-3 bg-white rounded-full opacity-40" id="dot6"></div>
         <div class="w-3 h-3 bg-white rounded-full opacity-40" id="dot7"></div>
-        <div class="w-3 h-3 bg-white rounded-full opacity-40" id="dot8"></div>
     </div>
 </div>
 
@@ -55,8 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('slide4'),
         document.getElementById('slide5'),
         document.getElementById('slide6'),
-        document.getElementById('slide7'),
-        document.getElementById('slide8')
+        document.getElementById('slide7')
     ];
     let dots = [
         document.getElementById('dot1'),
@@ -65,8 +61,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('dot4'),
         document.getElementById('dot5'),
         document.getElementById('dot6'),
-        document.getElementById('dot7'),
-        document.getElementById('dot8')
+        document.getElementById('dot7')
+
     ];
 
     let current = 0;
